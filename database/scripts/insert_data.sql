d98f842e1c451c54c1013c47a0fca75f157d3cb5
kmnsoo <wkdska22@naver.com>
2021-10-25 09:47:45 +0900

    DB add values-name

--- database/scripts/insert_data.sql
+++ database/scripts/insert_data.sql
@@ -2,3 +2,4 @@ INSERT INTO visits (visitor_name) VALUES ('John');
 INSERT INTO visits (visitor_name) VALUES ('Mark');
 INSERT INTO visits (visitor_name) VALUES ('Robin');
 INSERT INTO visits (visitor_name) VALUES ('Minsoo');
+INSERT INTO visits (visitor_name) VALUES ('Kane');
